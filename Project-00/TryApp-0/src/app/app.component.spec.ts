b46fbff2f9bff84bcc0d50b408e778edf9de31c5
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-04-19 19:35:53 -0300

    ajeitando o menu

--- Project-00/TryApp-0/src/app/app.component.spec.ts
+++ Project-00/TryApp-0/src/app/app.component.spec.ts
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-label');
     expect(menuItems.length).toEqual(12);
-    
+
     expect(menuItems[0].textContent).toContain('page');
 
 
--- Project-00/TryApp-0/src/app/app.component.ts
+++ Project-00/TryApp-0/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -11,14 +12,11 @@ import { IonicModule } from '@ionic/angular';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Pagina Inicial', url: '/folder/page', icon: 'mail' },
-      
-
-
-      ///aqui se guarda os objetos do menu
-
-
-
+    { title: 'Home', url: '/folder/home', icon: 'home' },
+    { title: 'Vendas', url: '/folder/vendas', icon: 'receipt' },
+    { title: 'Empréstimos', url: '/folder/emprestimos', icon: 'cash' },
+    { title: 'Recebíveis', url: '/folder/recebiveis', icon: 'wallet' },
+    { title: 'Conta', url: '/folder/conta', icon: 'person' },
   ];
   public labels = ['Rendimentos', 'Informações'];
   /// aqui as labels. ainda não sei como associar uma coisa a elas
