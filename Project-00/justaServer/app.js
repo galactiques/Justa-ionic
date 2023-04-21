d678cf2d23d147bc332748098df6b74c8ccef319
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-04-21 12:53:11 -0300

    ligação com o servidor funcionando

--- Project-00/justaServer/app.js
+++ Project-00/justaServer/app.js
@@ -1,17 +1,21 @@
-import express from 'express'
-import rotaVendas from '../justaServer/rotas/vendas.js'
-import rotaEmprestimo from '../justaServer/rotas/emprestimos.js'
+import express from 'express';
+import cors from 'cors'; // Importe a biblioteca cors
 
+import rotaVendas from '../justaServer/rotas/vendas.js';
+import rotaEmprestimo from '../justaServer/rotas/emprestimos.js';
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-const port = 8000
+const port = 8000;
 
-app.use('/vendas', rotaVendas)
-app.use('/emprestimo', rotaEmprestimo)
+// Configurar o CORS
+app.use(cors({
+    origin: 'http://localhost:8100' // Atualize com a origem permitida desejada
+}));
 
+app.use('/vendas', rotaVendas);
 
-app.listen(port, ()=>{
-    console.log("Escutando a porta 8000")
-})
+app.listen(port, () => {
+    console.log("Escutando a porta 8000");
+});
