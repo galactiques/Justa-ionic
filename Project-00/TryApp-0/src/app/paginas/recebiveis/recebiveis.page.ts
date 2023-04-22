2962df3b86910b92fb3b59ea709ee3678a569699
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-04-22 16:27:05 -0300

    lógica do backend dos recebíveis feita

--- Project-00/TryApp-0/src/app/paginas/recebiveis/recebiveis.page.ts
+++ Project-00/TryApp-0/src/app/paginas/recebiveis/recebiveis.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonButton } from '@ionic/angular';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
@@ -12,13 +12,15 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   imports: [IonicModule, CommonModule, FormsModule, HttpClientModule]
 })
 export class RecebiveisPage implements OnInit {
-  //saldo_em_conta: number = 32345.30;
-  //adiantamento: number = 130;
-
+  //public botaoJaClicado: boolean = false;
   public adiantar: number = 0;
   public saldo: number = 0;
-
-
+  public qtdvendas: number = 0;
+  public juros: number = 0;
+  public juros_percentual: number = 0;
+  public valor_taxa: number =0;
+  exibirTabela = false;
+  @ViewChild('botaoInformacoes', { static: false }) botaoInformacoes?: IonButton;
   constructor(private http: HttpClient) {}
 
 
@@ -26,19 +28,80 @@ export class RecebiveisPage implements OnInit {
     this.http.get('http://localhost:8000/vendas')
       .subscribe((response: any) => {
         //O quanto o usuário "Arthur" pode adiantar
-        this.adiantar= response.VendasLuan[12].valor
+        this.adiantar= response.VendasArthur[12].valor
       });
   }
 
   saldoEmConta(){
     this.http.get('http://localhost:8000/users')
     .subscribe((response:any)=>{
-      this.saldo = response.Users[0].saldo
+      //pegando o saldo do usuário "Arthur"
+      this.saldo = response.Users[2].saldo;
+    })
+  }
+
+  qtdDeVendas(){
+    this.http.get('http://localhost:8000/vendas')
+    .subscribe((response:any)=>{
+      this.qtdvendas = response.VendasArthur[12].vendas;
+      if (this.qtdvendas >= 1 && this.qtdvendas <= 4) {
+        this.juros = 1.02;
+        this.juros_percentual = 2;
+      } else if (this.qtdvendas >= 5 && this.qtdvendas <= 7) {
+        this.juros = 1.015;
+        this.juros_percentual = 1.5;
+      } else if (this.qtdvendas >= 8 && this.qtdvendas <= 14) {
+        this.juros = 1.01;
+        this.juros_percentual = 1;
+      } else {
+        this.juros = 1;
+        this.juros_percentual = 0;
+      }
     })
   }
   ngOnInit() {
-    this.obterVendadoServidor(),
+    this.obterVendadoServidor();
     this.saldoEmConta();
+    this.qtdDeVendas();
     }
 
-}
+    /*botaoClicado(){
+      if (!this.botaoJaClicado) {
+        const confirmar = window.confirm('Você tem certeza?');
+        if (confirmar) {
+          this.saldo += this.adiantar;
+          this.botaoJaClicado = true;
+        }
+      } else {
+        alert('O adiantamento já foi realizado, volte em 30 dias');
+      }
+    }*/
+
+    onClickInformacoes() {
+      // Altera o valor da variável de controle para mostrar ou ocultar a tabela
+      this.exibirTabela = !this.exibirTabela;
+    }
+
+
+    onPeriodoChange(event: any) {
+      const periodoSelecionado = event.detail.value;
+      switch (periodoSelecionado) {
+        case '1 mes':
+          this.valor_taxa = this.adiantar * (this.juros) - this.adiantar
+          break;
+        case '2 meses':
+          this.valor_taxa = this.adiantar * (this.juros ** 2) - this.adiantar
+          break;
+        case '3 meses':
+          this.valor_taxa = this.adiantar * (this.juros ** 3) - this.adiantar
+          break;
+        default:
+          this.valor_taxa = 0;
+          break;
+      }
+    }
+
+
+  }
+
+
