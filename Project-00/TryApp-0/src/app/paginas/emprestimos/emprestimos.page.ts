d3bbf0d3c7660993619ae3e9b43f20f43b5afb17
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-04-23 22:23:27 -0300

    pag empréstimos(lógica e cálculos funcionando)
    Falta apenas achar uma maneira de jogar esses dados no servidor já existente

--- Project-00/TryApp-0/src/app/paginas/emprestimos/emprestimos.page.ts
+++ Project-00/TryApp-0/src/app/paginas/emprestimos/emprestimos.page.ts
@@ -17,7 +17,12 @@ export class EmprestimosPage implements OnInit {
   public limite: number = 0;
   public juros: number = 1.03;
   public periodoSelecionado: string = '';
-
+  valor_digitado: number | null = null;
+  valorDigitado: number = 0;
+  public valor_final: number = 0;
+  public botaoJaClicado: boolean = false;
+  public meses: number = 0;
+  exibirTabela = false;
 
   constructor(private http: HttpClient) { }
 
@@ -33,30 +38,72 @@ export class EmprestimosPage implements OnInit {
     this.calculandoMediaMensal();
   }
 
+  onClickInformacoes() {
+    this.exibirTabela = !this.exibirTabela;
+  }
+
+  botaoClicado() {
+    if (!this.botaoJaClicado) {
+      if (this.valor_digitado !== null) {
+        if (isNaN(this.valorDigitado)) {
+          alert('Por favor, digite um valor numérico válido.');
+        } else if (this.valorDigitado === 0) {
+          alert('Por favor, digite um valor maior que zero.');
+        } else if (this.valorDigitado <= this.limite) {
+          const confirmar = window.confirm(`Confirmar empréstimo?`);
+          if (confirmar) {
+            /*somar valor no saldo do servidor
+            atribuir valor a ser pago do empréstimo no servidor ;*/
+            this.botaoJaClicado = true;
+          }
+        } else {
+          alert('O valor digitado é maior do que o permitido.');
+        }
+      } else {
+        alert('Por favor, digite um valor.');
+      }
+    } else {
+      alert('O empréstimo já foi realizado'); // Mensagem de alerta caso o botão já tenha sido clicado
+    }
+  }
+
+
+  atualizarValorDigitado(event: any) {
+    this.valor_digitado = parseFloat(event.target.value);
+    this.valorDigitado = Number(this.valor_digitado);
+    this.valor_final = this.valorDigitado * (this.juros ** this.meses);
+  }
 
   onPeriodoChange(event: any) {
     this.periodoSelecionado = event.detail.value;
     switch (this.periodoSelecionado) {
       case '1 mes':
         this.limite = this.media_mensal;
+        this.meses = 1;
         break;
       case '2 meses':
         this.limite = this.media_mensal * 2;
+        this.meses = 2;
         break;
       case '3 meses':
         this.limite = this.media_mensal * 3;
+        this.meses = 3;
         break;
       case '4 meses':
         this.limite = this.media_mensal * 4;
+        this.meses = 4;
         break;
       case '5 meses':
         this.limite = this.media_mensal * 5;
+        this.meses =5;
         break;
       case '6 meses':
         this.limite = this.media_mensal * 6;
+        this.meses = 6;
         break;
       default:
         this.limite = 0;
+        this.meses = 0;
         break;
     }
 
